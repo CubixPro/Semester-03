f499177e061fafa08651ef79676fff79c51e9f91
Titir Adhikary <titir.adhikary@hotmail.com>
2019-09-25 00:47:14 +0530

    3, 4, 5 of OOPs

--- OOP and Programming/Assignment 4/p3.cpp
+++ OOP and Programming/Assignment 4/p3.cpp
@@ -1,28 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
 class Array{
 	int *p;
 	int size;
 	public:
-	 Array(int size)
-	 {
+	Array(int size)
+	{
 	 	if(size >= 1)
 	 	{
 	 		p = new int[size];
+            this->size = size;
 	 		for(int i = 0 ; i < size ; i++)
 	 		{
-	 			cin >> p[i];
+                 p[i] = 0;
 	 		}
 	 	}
 	 	else
 	 	{
+            this->size = 0;
 	 		p = NULL;
 	 	}
-	 }
+	}
 	 ~Array()
-		{
+	{
 			if (p!= NULL)
 				delete(p);
-		}
-	Array(Array &b)
+	}
+	Array(const Array &b)
 	{
 		size = b.size;
 		if(size != 0)
@@ -30,35 +36,36 @@ class Array{
 			p = new int[size];
 		}
 		else 
-			p = null;
+			p = NULL;
 		for(int i = 0 ;  i < size ; i++)
 		{
 			p[i] = b.p[i];
 		}
 	}
-	Array operator=(Array t)
+	void operator=(const Array t)
 	{
-		size = a.size;
+		size = t.size;
 		if(size != 0)
 		{
 			p = new int[size];
 		}
 		else 
-			p = null;
+			p = NULL;
 		for(int i = 0 ;  i < size ; i++)
 		{
-			p[i] = b.p[i];
+			p[i] = t.p[i];
 		}
 	}
-	Array operator+(Array b)
+	Array operator+(const Array b)
 	{
 		if(size == b.size)
 		{
 			Array final_a(size);
 			for(int i = 0 ; i < size ; i++)
 			{
-				final_a[i] = p[i] + b.p[i];
+				final_a.p[i] = p[i] + b.p[i];
 			}
+            //final_a.display();
 			return final_a;
 		}
 		else
@@ -67,6 +74,19 @@ class Array{
 			Array final_a(0);
 			return final_a;
 		}
+		
+	}
+	Array operator*(int b)
+	{
+			Array final_a(size);
+			for(int i = 0 ; i < size ; i++)
+			{
+				final_a.p[i] = p[i] * b; 
+			}
+            //final_a.display();
+			return final_a;
+		
+		
 	}
 	int operator[](int k)
 	{
@@ -77,14 +97,47 @@ class Array{
 		else 
 		 return -1;
 	}
-}
+	void display()
+	{
+		if(size == 0)
+		{
+			printf("Null array");
+
+		}
+		else
+		{
+			for(int i = 0 ; i < size ; i++)
+			{
+				printf("%d ", p[i]);
+			}
+		}
+        printf("\n");
+	}
+    void input()
+    {
+        for(int i = 0 ; i < size ; i++)
+        {
+            cin >> p[i];
+        }
+    }
+	friend Array operator*(int k, Array b)
+	{
+		Array final_a(b.size);
+		for(int i = 0 ; i < b.size ; i++)
+		{
+			final_a.p[i] = k * b.p[i];
+		}
+		return final_a;
+	}
+};
 
 int main()
 {
-	Array a[5];
-	Array b[5];
-	Array c = a + b;
-	
-	
-		}
+	Array a(5);
+    a.input();
+	Array b(5);
+    b.input();
+    Array c = 4 * a * 4;
+	c.display();	
+}
 		
